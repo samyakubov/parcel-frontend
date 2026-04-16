66d228d706bc792deb67d1472c3868bca1d94be3
samyakubov <75036399+samyakubov@users.noreply.github.com>
2026-04-15 18:16:17 -0700

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next"
-import { Inter, Geist_Mono } from "next/font/google"
+import { Plus_Jakarta_Sans, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { ThemeProvider } from "next-themes"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
-const inter = Inter({
-	variable: "--font-inter",
+const plusJakartaSans = Plus_Jakarta_Sans({
+	variable: "--font-plus-jakarta-sans",
 	subsets: ["latin"],
 })
 
@@ -29,7 +29,7 @@ export default function RootLayout({
 		//TODO: figure out the hydration error from theme provider
 		<html lang="en" suppressHydrationWarning>
 			<body
-				className={`${inter.variable} ${geistMono.variable} antialiased`}
+				className={`${plusJakartaSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
 					{children}
